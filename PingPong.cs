b0f12580cdbd50b4c2df8dbaf72c640341870743
Alexander <knibz@protonmail.com>
2022-06-20 23:55:27 -0400

    add initial files

---
+++ PingPong.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public class Pong
+{
+  static void Main()
+  {
+    Console.WriteLine("Give me a number and I will ping pong it!");
+    string yourNumberString = Console.ReadLine();
+    int yourNumber = int.Parse(yourNumberString);
+  }
+}
+
+
+
+if (yourNumber%3 == 0) 
+{
+  return "ping"
+}
+
+if (yourNumber%5 == 0) 
+{
+  return "pong"
+}
+
+if (yourNumber%3 == 0 && yourNumber%5 == 0) 
+{
+  return "ping-pong"
+}
+
+
+  foreach (int i = 0; i <= number; i++) {
+  }
